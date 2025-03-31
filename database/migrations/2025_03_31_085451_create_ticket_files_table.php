b37d1b77c789c921835c5843cd98a10ee8fc7aa8
ilg2005 <36738148+ilg2005@users.noreply.github.com>
2025-03-31 11:58:58 +0300

    таблица ticket_files

---
+++ database/migrations/2025_03_31_085451_create_ticket_files_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ticket_files', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('ticket_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained();
+            $table->string('original_filename');
+            $table->string('filename')->unique();
+            $table->string('path');
+            $table->string('mime_type')->nullable();
+            $table->unsignedInteger('size');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ticket_files');
+    }
+};
