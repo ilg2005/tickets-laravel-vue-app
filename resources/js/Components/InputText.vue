3ede0eda087458b294d94062c5890c94a8bab5bd
ilg2005 <36738148+ilg2005@users.noreply.github.com>
2025-04-09 06:29:40 +0300

    рефакторинг

---
+++ resources/js/Components/InputText.vue
@@ -0,0 +1,14 @@
+<script setup>
+// Этот файл является заглушкой для обратной совместимости
+// Используйте TextInput вместо InputText
+import TextInput from '@/Components/TextInput.vue';
+
+const model = defineModel({
+    type: String,
+    required: true,
+});
+</script>
+
+<template>
+    <TextInput v-model="model" v-bind="$attrs" />
+</template> 
